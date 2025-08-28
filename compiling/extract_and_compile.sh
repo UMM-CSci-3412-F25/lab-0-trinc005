489e2dfd3af3b76113deb2bc248f1a4826b88aa5
Alex Trinciante <trinc005@morris.umn.edu>
2025-08-28 11:41:43 -0500

    Working On Last 2 Tests

--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -1,6 +1,37 @@
 #!/bin/bash
 
-#Extratcs the following ziped file into the working directory.
-tar -xzf NthPrime.tgz
+NTHPRIME="NthPrime.tgz"
+NUM=$1
+DIR="./NthPrime"
 
-cd /home/trinc005/Documents/lab-0-trinc005/compiling/NthPrime
+if [ -f "$NTHPRIME" ]; then
+  tar -xzf "$NTHPRIME"
+else
+  exit 1
+fi
+
+if [ -d "$DIR" ]; then
+  cd "$DIR"
+ # echo $PWD
+else
+  exit 1
+fi
+
+if [ -f "main.c" ]; then
+  gcc "main.c" "nth_prime.c" -o "NthPrime"
+  cd "./compiling"
+else
+  exit 1
+fi
+
+#EXECUTABLE="/home/trinc005/Documents/lab-0-trinc005/compiling/NthPrime/NthPrime"
+#ARG="$NUM"
+
+#if [ -n "$ARG" ]; then
+#  "$EXECUTABLE" "$ARG"
+#else
+ # exit 1
+#fi
+
+
+./NthPrime "$NUM"
